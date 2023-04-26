c0c3053e7626d166cd820947f8222c599d846737
maryclickap <mary@click-ap.com>
2023-04-26 10:24:30 +0800

    Add menu7

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -134,4 +134,5 @@ $autoload['language'] = array();
 */
 $autoload['model'] = array();
 array_push($autoload['libraries'], 'pagination');
+array_push($autoload['libraries'], 'form_validation');
 array_push($autoload['helper'], 'form');
